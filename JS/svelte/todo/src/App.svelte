81cefcdb6b40c399334d583fd47bd7d5e2a02e3e
Jong Cheon Park <simpleego@gmail.com>
2024-12-04 13:16:00 +0900

    Update App.svelte

--- JS/svelte/todo/src/App.svelte
+++ JS/svelte/todo/src/App.svelte
@@ -1,1 +1,35 @@
+<script>
+	import { writable } from 'svelte/store'
+	import Todo from './Todo.svelte'
 
+	let title = ''
+	let todos = writable([])
+	let id = 0
+	
+	function createTodo() {
+		if (!title.trim()) {
+		  title = ''
+			return
+ 	  }
+		$todos.push({
+			id,
+			title
+		})
+		$todos = $todos
+		title = ''
+		id += 1
+	}
+	// if (e.key === 'Enter') { createTodo() }
+	// e.key === 'Enter' ? createTodo() : undefined
+	// e.key === 'Enter' && createTodo()
+</script>
+
+<input bind:value={title}
+			 on:kepup={(e) => {e.key === 'Enter' && createTodo()}} />
+<button on:click={createTodo}>
+	Create Todo
+</button>
+
+{#each $todos as todo}
+	<Todo {todos} {todo} />
+{/each}
