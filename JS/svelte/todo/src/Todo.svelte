6e8f95bcf1799d4afbe75ddfabf2b9847e51801e
Jong Cheon Park <simpleego@gmail.com>
2024-12-04 13:16:40 +0900

    Update Todo.svelte

--- JS/svelte/todo/src/Todo.svelte
+++ JS/svelte/todo/src/Todo.svelte
@@ -1,1 +1,39 @@
+<script>
+	export let todos // Store!
+	export let todo
+	
+	let isEdit = false
+	let title = ''
+	
+	function onEdit() {
+		isEdit = true
+		title = todo.title
+	}
+	function offEdit() {
+		isEdit = false
+	}
+	function updateTodo() {
+		todo.title = title
+		$todos = $todos
+		offEdit()
+	}
+	function deleteTodo() {
+		$todos = $todos.filter(t => t.id !== todo.id)
+	}
+</script>
 
+{#if isEdit}
+	<div>
+		<input type="text"
+					 bind:value={title}
+					 on:keydown={(e) => {e.key === 'Enter' && updateTodo()}} />
+		<button on:click={updateTodo}>OK</button>
+		<button on:click={offEdit}>cancel</button>
+	</div>
+{:else}
+	<div>
+		<span>{todo.title}</span>
+		<button on:click={onEdit}>Edit</button>
+		<button on:click={deleteTodo}>Delete</button>
+	</div>
+{/if}
