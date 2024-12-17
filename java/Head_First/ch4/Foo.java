40b1837e9ba15497e9aff90b1508d0bf29248f47
Jong Cheon Park <simpleego@gmail.com>
2024-12-17 11:21:10 +0900

    Delete java/Head_First/ch4 directory

--- java/Head_First/ch4/Foo.java
+++
@@ -1,9 +0,0 @@
-package ch4;
-
-class Foo {
-  public void go() {
-    // intentionally doesn't compile
-//    int x;
-//    int z = x + 3;
-  }
-}
--- java/Head_First/ch4/Horse.java
+++
@@ -1,7 +0,0 @@
-package ch4;
-
-class Horse {
-  private double height = 15.2;
-  private String breed;
-  // more code...
-}
